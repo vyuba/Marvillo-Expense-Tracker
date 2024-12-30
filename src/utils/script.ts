16057dae04e4a1626fae59ee1fd3a5b8e35118dc
vyuba <preyealexander731@gmail.com>
2024-12-30 02:49:08 +0100

    cleared the script in utitls

--- src/utils/script.ts
+++ src/utils/script.ts
@@ -1,9 +1,9 @@
-import { useAppContext } from "../context/AppContext";
+// import { useAppContext } from "../context/AppContext";
 
-const { LoggedInUser, setLoggedInUser } = useAppContext();
-export const updateLoggedInUser = (updates) => {
-  setLoggedInUser((prevState) => ({
-    ...prevState,
-    ...updates,
-  }));
-};
+// const { setLoggedInUser } = useAppContext();
+// export const updateLoggedInUser = (updates) => {
+//   setLoggedInUser((prevState) => ({
+//     ...prevState,
+//     ...updates,
+//   }));
+// };
